1ca38a7a4338df78e64fac6e59f24760f0e57b71
hadiDanial <hadiDanial@users.noreply.github.com>
2022-12-21 02:05:01 +0200

    Added point rotation, update function to Point

--- Assets/_HadiSplines/Point.cs
+++ Assets/_HadiSplines/Point.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Hadi.Splines
@@ -6,6 +7,7 @@ namespace Hadi.Splines
     public class Point
     {
         public Vector3 anchor;
+        public Quaternion anchorRotation = Quaternion.identity;
         public Vector3 controlPoint1, controlPoint2;
         private Vector3 relativeControlPoint1, relativeControlPoint2;
         public ControlMode mode;
@@ -65,6 +67,11 @@ namespace Hadi.Splines
             this.mode = ControlMode.Aligned;
         }
 
+        public void SetRotation(Quaternion rotation)
+        {
+            anchorRotation = rotation;
+        }
+
         public void Refresh()
         {
             relativeControlPoint1 = controlPoint1 - anchor;
@@ -92,6 +99,28 @@ namespace Hadi.Splines
         {
             relativeControlPoint2 = -relativeControlPoint1;
             this.controlPoint2 = anchor - relativeControlPoint1;
+        }
+
+        public void Update(Vector3 anchor, Vector3 control1, Vector3 control2, Quaternion rotation)
+        {
+            bool refresh = false;
+            if(!this.anchor.Equals(anchor))
+            {
+                this.anchor = anchor;
+                this.controlPoint1 = anchor + relativeControlPoint1;
+                this.controlPoint2 = anchor + relativeControlPoint2;
+                refresh = true;
+            }
+            else if(!this.controlPoint1.Equals(control1) || !this.controlPoint2.Equals(control2) || !this.anchorRotation.Equals(rotation))
+            {
+                refresh = true;            
+                this.controlPoint1 = control1;
+                this.controlPoint2 = control2;
+                this.anchorRotation = rotation;
+            }
+
+            if (refresh)
+                Refresh();
         }
     }
 }
