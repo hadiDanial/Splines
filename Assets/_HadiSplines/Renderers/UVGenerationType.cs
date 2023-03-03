0019ac1abe3bc2142702135c79df6518ceaa9e8f
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-03 03:16:48 +0200

    Added UVGenerationType enum

---
+++ Assets/_HadiSplines/Renderers/UVGenerationType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hadi.Splines
+{
+    public enum UVGenerationType
+    {
+        Mesh,
+        Segment,
+        Length
+    }
+}
